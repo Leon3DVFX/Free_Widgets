4d25cc3e61616adc67421ec4e3539c385f0a09b5
Leonid <leonid.Borolis@icloud.com>
2026-04-26 15:40:24 +0500

    Update Widget

--- icon_timer.py
+++ icon_timer.py
@@ -24,20 +24,26 @@ class IconTimer(QtCore.QObject):
 
 # Класс виджет таймер - иконка + анимация
 class IconTimerWidget(QtWidgets.QWidget):
-    def __init__(self, parent=None, interval=1000, icon_path=None, height=16, pen_width = 1):
+    def __init__(self, parent=None, interval=1000, icon_path=None, height=16, pen_width = 1,
+                 pen_color=QtGui.QColor(255, 255, 255, 100), ring_gap=2):
         super().__init__(parent)
-        # Предварительная
+        # Предварительная настройка таймера
         self.interval = max(1, int(interval))
         self.remaining_ms = float(self.interval)
+        self._icon_height = max(1, int(height))
+        self.ring_gap = max(0, int(ring_gap))
+        # Только размеры виджета и полная прозрачность
+        self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
 
-        self.icon = QtGui.QPixmap(icon_path) if icon_path else None
-        if self.icon is not None:
-            self.icon = self.icon.scaledToHeight(
-                height,
-                mode=QtCore.Qt.TransformationMode.SmoothTransformation,
-            )
+        self._icon_source = QtGui.QPixmap(icon_path) if icon_path else QtGui.QPixmap()
+        self.icon = QtGui.QPixmap()
+
         # Настройка пена (предварительно) TODO: Сделать настраиваемо
-        self.pen = QtGui.QPen(QtGui.QColor(255, 255, 255, 100), pen_width)
+        self.pen = QtGui.QPen(pen_color, max(1, int(pen_width)))
+        widget_size = self._icon_height + (2 * self.ring_gap) + (2 * self.pen.width())
+        self.resize(widget_size, widget_size)
+        self._rescale_icon()
 
         self.tick = QtCore.QTimer(self)
         self.tick.setTimerType(QtCore.Qt.TimerType.PreciseTimer)
@@ -47,6 +53,26 @@ class IconTimerWidget(QtWidgets.QWidget):
         self.elapsed = QtCore.QElapsedTimer()
         self.elapsed.start()
         self.tick.start()
+
+    def _arc_rect(self):
+        half_pen = self.pen.widthF() / 2.0
+        return QtCore.QRectF(self.rect()).adjusted(half_pen, half_pen, -half_pen, -half_pen)
+
+    def _rescale_icon(self):
+        if self._icon_source.isNull():
+            self.icon = QtGui.QPixmap()
+            return
+
+        available_height = self._arc_rect().height() - self.pen.widthF() - (2 * self.ring_gap)
+        target_height = max(1, int(round(min(self._icon_height, available_height))))
+        self.icon = self._icon_source.scaledToHeight(
+            target_height,
+            mode=QtCore.Qt.TransformationMode.SmoothTransformation,
+        )
+
+    def resizeEvent(self, event):
+        self._rescale_icon()
+        super().resizeEvent(event)
     # Отсчет до перерисовки
     def _on_tick(self):
         self.remaining_ms = max(0, self.interval - self.elapsed.elapsed())
@@ -54,25 +80,27 @@ class IconTimerWidget(QtWidgets.QWidget):
 
         if self.remaining_ms == 0:
             self.tick.stop()
-            self.update_icon()
+            self.time_out()
     # Метод TimeOut таймера
-    def update_icon(self):
+    def time_out(self):
         print("Time out")
         self.close()
+
     # Перерисовка виджета
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
-        painter.setRenderHints(QtGui.QPainter.RenderHint.Antialiasing)
+        painter.setRenderHints(
+            QtGui.QPainter.RenderHint.Antialiasing
+            | QtGui.QPainter.RenderHint.SmoothPixmapTransform
+        )
         painter.setPen(self.pen)
         painter.setBrush(QtCore.Qt.BrushStyle.NoBrush)
+        rect = self._arc_rect()
         # Процедурный rect - пересчет
-        size = max(8, min(self.width(), self.height()) - self.pen.width())
-        rect = QtCore.QRect(
-            (self.width() - size) // 2,
-            (self.height() - size) // 2,
-            size,
-            size,
-        )
+        if not self.icon.isNull():
+            icon_x = int(round(rect.center().x() - (self.icon.width() / 2)))
+            icon_y = int(round(rect.center().y() - (self.icon.height() / 2)))
+            painter.drawPixmap(icon_x, icon_y, self.icon)
 
         progress = self.remaining_ms / self.interval
         start_angle = -270 * 16
@@ -83,7 +111,9 @@ class IconTimerWidget(QtWidgets.QWidget):
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    win = IconTimerWidget(interval=1500, pen_width=15)
-    win.resize(200,200)
+    win = IconTimerWidget(interval=3000, pen_width=3, pen_color=QtGui.QColor(243, 75, 105, 150), height=50,
+                          ring_gap=0,
+                          icon_path=r"C:\Users\User\Desktop\icons_test\icons\py_active.png")
+    win.show()
     win.show()
     sys.exit(app.exec())
